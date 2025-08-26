b2e59d2998552af4463b78fc53709f668c14a30d
Mar1155 <mariobarbieri11114@gmail.com>
2025-08-26 13:09:42 +0200

    1-chip8-logo.ch8 test works

--- emulator.c
+++ emulator.c
@@ -1,6 +1,8 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
+#include <string.h>
+#include <time.h>
 /*
 unsigned char V[16] --> 16 * 8-bit registers
     * they are referred as Vx. x --> hex decimal digit (0 --> f)
@@ -39,6 +41,7 @@ the display state/data is stored in [0x000 to 0x1FF] 32 x 64 bytes
 */
 struct chip_8 {
     uint8_t V[16];
+    uint8_t VF;
     uint16_t I;
     uint8_t delay;
     uint8_t sound;
@@ -48,11 +51,36 @@ struct chip_8 {
     uint8_t memory[4096];
 };
 
+uint8_t chip8_fontset[80] = {
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
+
+void load_fontset(struct chip_8 *arch) {
+    for (int i = 0; i < 80; i++)
+        arch->memory[i] = chip8_fontset[i];
+}
+
 /*
 Read the memory section which contains the display state and draw it in terminal with ANSI codes
 */
 void draw_display(struct chip_8 *arch) {
-    // printf("\033[?25l"); // hide cursor
+    printf("\033[?25l"); // hide cursor
+    printf("\033[H\033[2J");
     fflush(stdout);
     for (int y = 0; y < 32; y++) {     // row
         for (int x = 0; x < 64; x++) { // columns
@@ -66,7 +94,7 @@ void draw_display(struct chip_8 *arch) {
             }
         }
     }
-    // printf("\033[?25h"); // show cursor
+    printf("\033[?25h"); // show cursor
     fflush(stdout);
 }
 
@@ -75,19 +103,20 @@ Set the memory section relative to display state to 0x00
 */
 void clear_display(struct chip_8 *arch) {
     for (int i = 0; i < 256; i++) {
-        arch->memory[i] = 0x00; // the first 256 bytes of memory --> display state setted to 0
+        arch->memory[i] = 0; // the first 256 bytes of memory --> display state setted to 0
     }
 }
 
 void init_arch(struct chip_8 *arch) {
-    arch->I = 0x00;
-    arch->delay = 0x00;
-    arch->sound = 0x00;
+    arch->I = 0;
+    arch->delay = 0;
+    arch->sound = 0;
     arch->SP = 0;
     arch->PC = 0x200;
     for (int i = 0; i < 4096; i++) {
         arch->memory[i] = 0x00;
     }
+    load_fontset(arch);
 }
 
 /*
@@ -102,17 +131,14 @@ int read_program(struct chip_8 *arch, char *program_path) {
     filelen = ftell(fileptr);            // Get the current byte offset in the file
     rewind(fileptr);                     // Jump back to the beginning of the file
 
-    fread(&arch->memory[512], 1, filelen, fileptr); // Read in the entire file
+    fread(&arch->memory[0x200], 1, filelen, fileptr); // Read in the entire file
     fclose(fileptr);
     return filelen;
 }
 
-unsigned short get_PC_inst(struct chip_8 *arch) {
+unsigned short fetch_instr(struct chip_8 *arch) {
     // high | low (1 bytes each) of 16 bit instructions
-    uint8_t high = arch->memory[arch->PC];
-    uint8_t low = arch->memory[arch->PC + 1];
-    uint16_t inst = (high << 8) | low;
-    return inst;
+    return (arch->memory[arch->PC] << 8) | arch->memory[arch->PC + 1];
 }
 
 int main(int argv, char **args) {
@@ -128,34 +154,88 @@ int main(int argv, char **args) {
     char *program_path = args[1];
     long filelen = read_program(&arch, program_path);
 
-    uint16_t inst = get_PC_inst(&arch);
-    printf("%04X", inst);
-
     int running = 1;
-
+    int n_instr = 0;
+    int incr = 1;
     while (running) {
-        switch (inst & 0xF000) {
-            case 0x0000:
-                switch (inst) {
-                    case 0x00E0:
-                        printf("run 00E0\n");
-                        clear_display(&arch);
-                        break;
-                    case 0x0F02:
-                        printf("run 0F02 ignored\n");
-                        break;
-                    default:
-                        printf("inst %04X not implemented\n", inst);
-                        running = 0;
-                        break;
-                }
-                break;
-
-            // altri casi
-        }
-        // draw_display(&arch);
+        uint16_t inst = fetch_instr(&arch);
         arch.PC += 2;
-        inst = get_PC_inst(&arch);
+
+        printf("%d:  %04X --> ", n_instr, inst);
+
+        if (inst == 0x00E0) {
+            clear_display(&arch);
+            printf("CLS\n");
+        } else if (inst == 0x00EE) {
+           arch.PC = arch.stack[--arch.SP];
+        } else if ((inst & 0xF000) == 0x0000) { // ignored by modern interpreters
+            // uint16_t nnn = (inst >> 4) & 0xFFF;
+            // arch.PC = nnn;
+            printf("%04X ignored\n", inst);
+        } else if ((inst & 0xF000) == 0x1000) {
+            uint16_t nnn = inst & 0xFFF;
+            arch.PC = nnn;
+            printf("JP %04X\n", nnn);
+        } else if ((inst & 0xF000) == 0x2000) {
+            uint16_t nnn = inst & 0xFFF;
+            arch.stack[arch.SP] = arch.PC;
+            arch.SP++;
+            arch.PC = nnn;
+            printf("CALL %03X", nnn);
+        } else if ((inst & 0xF000) == 0x6000) {
+            uint8_t x = (inst >> 8) & 0xF; // register index
+            uint8_t kk = inst & 0xFF;      // 8 bit lowest bits of the instruction
+            printf("LD V%d, %02X\n", x, kk);
+            arch.V[x] = kk;
+        } else if ((inst & 0xF000) == 0x7000) {
+            uint8_t x = (inst >> 8) & 0xF; // register index
+            uint8_t kk = inst & 0xFF;
+            arch.V[x] += kk;
+            printf("ADD V%X, %X", x, kk);
+        } else if ((inst & 0xF000) == 0xA000) {
+            uint16_t nnn = inst & 0xFFF;
+            arch.I = nnn;
+            printf("LD I, %04X\n", nnn);
+        } else if ((inst & 0xF000) == 0xD000) {
+            uint8_t x = (inst >> 8) & 0xF;
+            uint8_t y = (inst >> 4) & 0xF;
+            uint8_t n = inst & 0xF;
+            uint8_t x_coord = arch.V[x] % 64;
+            uint8_t y_coord = arch.V[y] % 32;
+            arch.VF = 0;
+
+            for (uint8_t row = 0; row < n; row++) {
+                uint8_t sprite_byte = arch.memory[arch.I + row];                    // read byte memory
+                for (uint8_t col = 0; col < 8; col++) {                             // iterate over the 8 bits's byte
+                    uint8_t sprite_pixel = (sprite_byte >> (7 - col)) & 1;          // read col_th sprite bit
+                    uint16_t screen_index = (y_coord + row) * 64 + (x_coord + col); // get screen coordinates
+                    uint8_t *screen_pixel = &arch.memory[screen_index / 8];         // prev screen byte
+                    uint8_t bit_pos = 7 - (screen_index % 8);                       // prev screen bit position
+
+                    // XOR del pixel
+                    uint8_t prev = (*screen_pixel >> bit_pos) & 1; // prev screen pixel
+                    *screen_pixel ^= sprite_pixel << bit_pos;      // xor with new pixel, to check collisions
+
+                    if (prev && sprite_pixel) {
+                        arch.VF = 1; // set collision flag to one
+                    }
+                }
+            }
+            printf("\n");
+        } else if ((inst & 0xF000) == 0xE000) {
+            printf("E000 mask\n");
+        }
+        // not implemented branch
+        else {
+            printf("%04X Not implemented", inst);
+            return 1;
+        }
+        fflush(stdout);
+
+        // draw_display(&arch)
+        n_instr++;
+        struct timespec ts = {.tv_sec = 0, .tv_nsec = 16666666}; // 60 fps
+        nanosleep(&ts, NULL);
     }
 
     return 0;
